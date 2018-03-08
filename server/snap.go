5929d78e5c427c4a99d3739fc1fcf7692ecd0dad
mellowd <mellow.drifter@gmail.com>
2018-03-07 21:16:29 -0500

    Adjust name of service. Add initial server code

---
+++ server/snap.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/mellowdrifter/snapshot/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	log.Println("Listening on port 5001")
+	lis, err := net.Listen("tcp", ":5001")
+	if err != nil {
+		log.Fatalf("Failed to bind: %v,", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSnapShotServer(grpcServer, &server{})
+
+	grpcServer.Serve(lis)
+}
+
+func (s *server) AddSnap(ctx context.Context, id *pb.ImageData) (*pb.Result, error) {
+	log.Printf("Received something")
+	return &pb.Result{}, nil
+}
